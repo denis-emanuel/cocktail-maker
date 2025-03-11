54fca642966ba45927e85b651438448fe4160bdd
Denis Pop <pop.denis1902@gmail.com>
2025-03-11 19:55:05 +0200

    auto format scale.h

--- include/scale.h
+++ include/scale.h
@@ -4,45 +4,55 @@
 #include "HX711.h"
 #include "./constants.h"
 
-class ScaleController {
-    private:
-        HX711 scale;
-        unsigned int readingCounter = 0;
+class ScaleController
+{
+private:
+    HX711 scale;
+    unsigned int readingCounter = 0;
 
-    public:
-        ScaleController() {}
-        
-        void begin() {
-            this->scale = HX711();
-            this->scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-            this->scale.set_scale(397.f);
-            this->scale.tare();
+public:
+    ScaleController() {}
+
+    void begin()
+    {
+        this->scale = HX711();
+        this->scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+        this->scale.set_scale(397.f);
+        this->scale.tare();
+    }
+
+    int read()
+    {
+        if (this->scale.is_ready())
+        {
+            return this->scale.get_units(10);
         }
-
-        int read() {
-            if (this->scale.is_ready()) {
-                return this->scale.get_units(10);
-            } else {
-                return -1;
-            }
+        else
+        {
+            return -1;
         }
+    }
 
-        void tare() {
-            this->scale.tare();
-        }
+    void tare()
+    {
+        this->scale.tare();
+    }
 
-        void incrementReadingCounter() {
-            this->readingCounter++;
-            Serial.println("Reading counter incremented");
-        }
+    void incrementReadingCounter()
+    {
+        this->readingCounter++;
+        Serial.println("Reading counter incremented");
+    }
 
-        int getReadingCounter() {
-            return this->readingCounter;
-        }   
+    int getReadingCounter()
+    {
+        return this->readingCounter;
+    }
 
-        void resetReadingCounter() {
-            this->readingCounter = 0;
-        }     
+    void resetReadingCounter()
+    {
+        this->readingCounter = 0;
+    }
 };
 
 #endif
